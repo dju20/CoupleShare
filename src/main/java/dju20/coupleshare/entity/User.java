a1091c1bbe7776b309fd06d0c281e1c5f3f63296
suhyun9764 <suhyun9764@naver.com>
2024-07-27 02:22:05 +0900

    feat(): User builder 추가

--- src/main/java/dju20/coupleshare/entity/User.java
+++ src/main/java/dju20/coupleshare/entity/User.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -25,5 +26,18 @@ public class User {
 	private String email;
 	private String provider;
 	private String role;
+
+	@Builder
+	public User(String username, String sex, String password, String realName, String profileImg, String email, String provider, String role) {
+		this.username = username;
+		this.sex = sex;
+		this.password = password;
+		this.realName = realName;
+		this.profileImg = profileImg;
+		this.email = email;
+		this.provider = provider;
+		this.role = role;
+	}
+
 
 }
