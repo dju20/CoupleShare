4e55c03690a1e6a62341da8d1b94c51cb7b8531a
suhyun9764 <suhyun9764@naver.com>
2024-07-27 01:51:46 +0900

    feat(): UsersRepository 생성

---
+++ src/main/java/dju20/coupleshare/repository/UsersRepository.java
@@ -0,0 +1,8 @@
+package dju20.coupleshare.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import dju20.coupleshare.entity.User;
+
+public interface UsersRepository extends JpaRepository<User, Long> {
+}
