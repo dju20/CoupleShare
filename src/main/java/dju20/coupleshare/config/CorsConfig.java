9e846e2a03c355c9b2f9572f3c6c2853f1f2e129
suhyun9764 <suhyun9764@naver.com>
2024-07-27 00:37:31 +0900

    chore(): config 디렉토리로 이동

--- src/main/java/dju20/coupleshare/config/CorsConfig.java
+++ src/main/java/dju20/coupleshare/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package dju20.coupleshare;
+package dju20.coupleshare.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
