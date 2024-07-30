77ac16bf247f1675f468425eb31c97a26034bdcf
suhyun9764 <suhyun9764@naver.com>
2024-07-31 00:57:29 +0900

    feat(): jwt 구현

--- src/main/java/dju20/coupleshare/jwt/LoginFilter.java
+++ src/main/java/dju20/coupleshare/jwt/LoginFilter.java
@@ -1,15 +1,19 @@
 package dju20.coupleshare.jwt;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Iterator;
 
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import dju20.coupleshare.dto.CustomUserDetails;
 import dju20.coupleshare.dto.users.LoginDto;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.http.HttpServletRequest;
@@ -20,6 +24,7 @@ import lombok.RequiredArgsConstructor;
 public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
 	private final AuthenticationManager authenticationManager;
+	private final JwtUtil jwtUtil;
 
 	@Override
 	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
@@ -44,11 +49,22 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
 	@Override
 	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain, Authentication authentication){
-		System.out.println("Success");
+		CustomUserDetails customUserDetails = (CustomUserDetails)authentication.getPrincipal();
+		String username = customUserDetails.getUsername();
+
+		Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+		Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+		GrantedAuthority auth = iterator.next();
+
+		String role = auth.getAuthority();
+
+		String token = jwtUtil.createJwt(username,role,3600000L);
+
+		response.addHeader("Authorization","Bearer "+token);
 	}
 
 	@Override
 	protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed){
-		System.out.println("fail");
+		response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 	}
 }
