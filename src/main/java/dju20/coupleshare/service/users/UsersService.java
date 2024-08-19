15461717d0c6d90748555cd4792e3873b3a7b261
suhyun9764 <suhyun9764@naver.com>
2024-08-19 20:57:33 +0900

    refactor: GetMapping으로 변경으로 인한 pathvariable로 변경

--- src/main/java/dju20/coupleshare/service/users/UsersService.java
+++ src/main/java/dju20/coupleshare/service/users/UsersService.java
@@ -1,14 +1,14 @@
 package dju20.coupleshare.service.users;
 
-import java.util.Optional;
-
 import dju20.coupleshare.dto.users.register.RegisterDto;
 import dju20.coupleshare.dto.users.response.UserResponseDto;
-import dju20.coupleshare.dto.users.search.FindUserByFriendCodeDto;
+import java.util.Optional;
 
 public interface UsersService {
-	void saveUser(RegisterDto registerDto);
-	Optional<UserResponseDto> findUserByFriendCode(FindUserByFriendCodeDto findUserByFriendCodeDto);
+
+    void saveUser(RegisterDto registerDto);
+
+    Optional<UserResponseDto> findUserByFriendCode(String friendCode);
 
 
 }
