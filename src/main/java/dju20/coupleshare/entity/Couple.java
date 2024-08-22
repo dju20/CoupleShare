a4b7c8d531aa191baeb841bd84eadb4fce84b46f
suhyun9764 <suhyun9764@naver.com>
2024-08-22 21:55:55 +0900

    refactor: User, Couple 일대일 매핑으로 변경

--- src/main/java/dju20/coupleshare/entity/Couple.java
+++ src/main/java/dju20/coupleshare/entity/Couple.java
@@ -1,20 +1,15 @@
 package dju20.coupleshare.entity;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import java.util.ArrayList;
-import java.util.List;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
 @Entity
 @Getter
@@ -22,13 +17,19 @@ import lombok.RequiredArgsConstructor;
 @AllArgsConstructor
 @Builder
 
-
 public class Couple {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToMany(mappedBy = "couple")
-    private List<User> users = new ArrayList<>();
+    @OneToOne
+    @JoinColumn(name = "user1_id")
+    private User user1;
+
+    @OneToOne
+    @JoinColumn(name = "user2_id")
+    private User user2;
+
+    private String specialNotes;
 
 }
--- src/main/java/dju20/coupleshare/entity/User.java
+++ src/main/java/dju20/coupleshare/entity/User.java
@@ -6,19 +6,13 @@ import dju20.coupleshare.enums.user.UserRole;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Transient;
-import java.util.Optional;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @Getter
@@ -26,51 +20,51 @@ import lombok.Setter;
 @AllArgsConstructor
 @Builder
 public class User {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	private String username;
-	@Enumerated(EnumType.STRING)
-	private Sex sex;
+    private String username;
+    @Enumerated(EnumType.STRING)
+    private Sex sex;
 
-	private String password;
-	private String realName;
-	private String profileImg;
-	private String email;
-	private String provider;
-	private String providerId;
-	@Enumerated(EnumType.STRING)
-	private CoupleStatus coupleStatus;
+    private String password;
+    private String realName;
+    private String profileImg;
+    private String email;
+    private String provider;
+    private String providerId;
+    @Enumerated(EnumType.STRING)
+    private CoupleStatus coupleStatus;
 
-	@Enumerated(EnumType.STRING)
-	private UserRole role;
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
 
-	public void changeCoupleStatusToWAIT() {
-		this.coupleStatus = CoupleStatus.WAIT;
-	}
+    public void changeCoupleStatusToWAIT() {
+        this.coupleStatus = CoupleStatus.WAIT;
+    }
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "couple_id")
-	private Couple couple;
+    public void changeCoupleStatusToCouple() {
+        this.coupleStatus = CoupleStatus.COUPLE;
+    }
 
-	// @Builder
-	// public User(String username, Sex sex, String password, String realName, String profileImg, String email, String provider, String providerId, Boolean isCouple, UserRole role) {
-	// 	this.username = username;
-	// 	this.sex = sex;
-	// 	this.password = password;
-	// 	this.realName = realName;
-	// 	this.profileImg = profileImg;
-	// 	this.email = email;
-	// 	this.provider = provider;
-	// 	this.providerId = providerId;
-	// 	this.isCouple = isCouple;
-	// 	this.role = role;
-	// }
+    public void changeCoupleStatusToSolo() {
+        this.coupleStatus = CoupleStatus.SOLO;
+    }
 
-	@Transient
-	public Optional<Couple> getCouple() {
-		return Optional.ofNullable(couple);
-	}
+    // @Builder
+    // public User(String username, Sex sex, String password, String realName, String profileImg, String email, String provider, String providerId, Boolean isCouple, UserRole role) {
+    // 	this.username = username;
+    // 	this.sex = sex;
+    // 	this.password = password;
+    // 	this.realName = realName;
+    // 	this.profileImg = profileImg;
+    // 	this.email = email;
+    // 	this.provider = provider;
+    // 	this.providerId = providerId;
+    // 	this.isCouple = isCouple;
+    // 	this.role = role;
+    // }
+
 
 }
