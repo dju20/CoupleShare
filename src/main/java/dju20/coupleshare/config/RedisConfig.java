75ae8311bf845d70c0f1ffa4dc091d09bc3aad65
suhyun9764 <suhyun9764@naver.com>
2024-08-22 19:23:07 +0900

    feat: redis 설정

--- src/main/java/dju20/coupleshare/config/RedisConfig.java
+++ src/main/java/dju20/coupleshare/config/RedisConfig.java
@@ -1,17 +1,20 @@
 package dju20.coupleshare.config;
 
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
+@Configuration
 public class RedisConfig {
+
     @Value("${spring.data.redis.host}")
     private String host;
 
-    @Value("{spring.data.redis.port}")
+    @Value("${spring.data.redis.port}")
     private int port;
 
     @Bean
@@ -20,11 +23,10 @@ public class RedisConfig {
     }
 
     @Bean
-    public RedisTemplate<?, ?> redisTemplate() {
-        RedisTemplate<?, ?> template = new RedisTemplate<>();
+    @Primary
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(redisConnectionFactory());
         return template;
     }
-
-
 }
