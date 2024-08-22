b476eedf23b164a9b1249bb4f09261503d77b9bf
suhyun9764 <suhyun9764@naver.com>
2024-08-22 21:57:50 +0900

    feat: 커플 매칭 1차 구현

--- src/main/java/dju20/coupleshare/service/couple/CoupleServiceImpl.java
+++ src/main/java/dju20/coupleshare/service/couple/CoupleServiceImpl.java
@@ -1,19 +1,17 @@
 package dju20.coupleshare.service.couple;
 
-import dju20.coupleshare.entity.Couple;
-import dju20.coupleshare.repository.CoupleRepository;
 import dju20.coupleshare.dto.couple.CoupleCodeRequestDto;
+import dju20.coupleshare.entity.Couple;
 import dju20.coupleshare.entity.User;
 import dju20.coupleshare.enums.user.CoupleStatus;
 import dju20.coupleshare.exception.user.LoginUserException;
+import dju20.coupleshare.repository.CoupleRepository;
 import dju20.coupleshare.repository.UserRepository;
 import dju20.coupleshare.util.CoupleCodeGenerator;
 import jakarta.transaction.Transactional;
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 
@@ -21,40 +19,97 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CoupleServiceImpl implements CoupleService {
     private final UserRepository userRepository;
-    private final RedisTemplate redisTemplate;
+    private final StringRedisTemplate redisTemplate;
     private final CoupleRepository coupleRepository;
+    private final ValueOperations valueOperations = redisTemplate.opsForValue();
 
     @Override
     @Transactional
     public String generateCoupleCode(String username) {
-        User user = userRepository.findByUsername(username).orElseThrow(() ->
+        User user = getUserByUsername(username);
+
+        validationIsCoupleUser(user);
+
+        if (user.getCoupleStatus() == CoupleStatus.WAIT) {
+            return valueOperations.get(user.getId().toString()).toString();
+        }
+
+        String coupleCode = getUniqueCoupleCode(valueOperations);
+        saveCoupleCode(valueOperations, coupleCode, user);
+        return coupleCode;
+    }
+
+    private User getUserByUsername(String username) {
+        return userRepository.findByUsername(username).orElseThrow(() ->
                 new LoginUserException());
+    }
 
-        ValueOperations valueOperations = redisTemplate.opsForValue();
-        if(user.getCoupleStatus()!= CoupleStatus.SOLO)
-            throw new IllegalArgumentException();
-
-        try {
-            String coupleCode = getUniqueCoupleCode(valueOperations, user);
-            return coupleCode;
-        }catch (Exception e){
+    private static void validationIsCoupleUser(User user) {
+        if (user.getCoupleStatus() == CoupleStatus.COUPLE) {
             throw new IllegalArgumentException();
         }
     }
 
-    private String getUniqueCoupleCode(ValueOperations valueOperations, User user) {
+    private String getUniqueCoupleCode(ValueOperations valueOperations) {
         String coupleCode;
         do {
             coupleCode = CoupleCodeGenerator.generate(10);
-        }while (valueOperations.get(coupleCode)!=null);
+        } while (valueOperations.get(coupleCode) != null);
 
-        valueOperations.set(coupleCode,user.getId(),Duration.ofDays(1));
-        user.changeCoupleStatusToWAIT();
         return coupleCode;
+    }
+
+    private static void saveCoupleCode(ValueOperations valueOperations, String coupleCode, User user) {
+        try {
+            valueOperations.set(coupleCode, user.getId().toString(), Duration.ofDays(1));
+            valueOperations.set(user.getId().toString(), coupleCode, Duration.ofDays(1));
+            user.changeCoupleStatusToWAIT();
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
     }
 
     @Override
     public void matchCouple(String username, CoupleCodeRequestDto coupleCodeRequestDto) {
+        User user = getUserByUsername(username);
 
+        String coupleCodeCreatorId = valueOperations.get(coupleCodeRequestDto.getCoupleCode()).toString();
+        validateCoupleCodeCreatorId(coupleCodeCreatorId);
+        User coupleCodeCreator = getUserById(coupleCodeCreatorId);
+
+        createAndSaveCouple(coupleCodeCreator, user);
+        removeCoupleCodeFromRedis(coupleCodeRequestDto, coupleCodeCreatorId);
     }
+
+    private static void validateCoupleCodeCreatorId(String coupleCodeCreatorId) {
+        if (coupleCodeCreatorId == null) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private User getUserById(String coupleCodeCreatorId) {
+        User coupleCodeCreator = userRepository.findById(Long.parseLong(coupleCodeCreatorId)).orElseThrow(() ->
+                new IllegalArgumentException());
+        return coupleCodeCreator;
+    }
+
+    private void createAndSaveCouple(User coupleCodeCreator, User user) {
+        Couple couple = Couple.builder()
+                .user1(coupleCodeCreator)
+                .user2(user)
+                .build();
+        try {
+            user.changeCoupleStatusToCouple();
+            coupleCodeCreator.changeCoupleStatusToCouple();
+            coupleRepository.save(couple);
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void removeCoupleCodeFromRedis(CoupleCodeRequestDto coupleCodeRequestDto, String coupleCodeCreatorId) {
+        redisTemplate.delete(coupleCodeCreatorId);
+        redisTemplate.delete(coupleCodeRequestDto.getCoupleCode());
+    }
+
 }
